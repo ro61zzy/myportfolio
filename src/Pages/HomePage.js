bda221bbcfd202de5cc6852e770b19c77a10cfdc
Rose Wachuka <67165120+ro61zzy@users.noreply.github.com>
2023-01-19 11:54:05 +0300

    AppBar

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DrawerAppBar from '../components/DrawerAppBar'
+
+function HomePage() {
+  return (
+    <div>
+        <DrawerAppBar />
+    </div>
+  )
+}
+
+export default HomePage
