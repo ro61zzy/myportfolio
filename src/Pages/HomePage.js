0710e4973abd1ba63e881e484d1740e544539a1f
nicolaskiprop <yegonicholas@gmail.com>
2023-02-01 19:10:34 +0300

    cards

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,9 +1,9 @@
 import React from "react";
-// import DrawerAppBar from "../components/Appbar/DrawerAppBar";
-// import About from "../components/AboutMe/About";
-// import Skills from "../components/Skills/Skills"
-// import Home from "../components/Hero/Home";
-// import Project from "../components/Projects/Project";
+import DrawerAppBar from "../components/Appbar/DrawerAppBar";
+import About from "../components/AboutMe/About";
+import Skills from "../components/Skills/Skills"
+import Home from "../components/Hero/Home";
+import Project from "../components/Projects/Project";
 import Services from "../components/Services/Services";
 import Index from "../components/GetInTouch/Index";
 
@@ -11,11 +11,11 @@ import Index from "../components/GetInTouch/Index";
 function HomePage() {
   return (
     <>
-      {/* <DrawerAppBar />
+      <DrawerAppBar />
       <Home />
       <About />
       <Skills />
-      <Project /> */}
+      <Project />
       <Services />
       <Index />
     </>
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -13,85 +13,143 @@ import "./Services.scss";
 function Services() {
   return (
     <Container maxWidth="xl">
-      <Box className="services" sx={{ fontSize: { xs: "22px", sm: "64px" } }}>
+      <Box className="services" sx={{ fontSize: { xs: "22px", sm: "64px" }, mb:'150px', mt:'100px' }}>
         Services
       </Box>
       {/* <Box className='wrap'></Box> */}
 
       <Grid container spacing={2}>
         <Grid item xs={4}>
-          <div class="card">
-            <div class="card__side card__side--front">
-              <div class="card__picture card__picture--1">&nbsp;</div>
-              <p class="card__price-value">ux/ui</p>
-              <p class="card__price-only">view more</p>
-            </div>
-            <div class="card__side card__side--back card__side--back-1">
-              <div class="card__cta">
-                <div class="card__price-box">
-                  <p class="card__price-only">only</p>
-                  <p class="card__price-value">$297</p>
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <div className="card">
+              <div className="card__side card__side--front">
+                <div className="card__picture card__picture--1">&nbsp;</div>
+                <p className="card__price-value">ux/ui</p>
+                <p className="card__price-only">view more</p>
+              </div>
+              <div className="card__side card__side--back card__side--back-1">
+                <div className="card__cta">
+                  <div className="card__price-box">
+                    <p className="card__price-only">only</p>
+                    <p className="card__price-value">$297</p>
+                  </div>
+                  <a href="#" className="btn btn--white">
+                    Book now
+                  </a>
                 </div>
-                <a href="#" class="btn btn--white">
-                  Book now
-                </a>
               </div>
             </div>
-          </div>
+          </Box>
         </Grid>
         <Grid item xs={4}>
-          <div class="card">
-            <div class="card__side card__side--front--2">
-              <div class="card__picture card__picture--1">&nbsp;</div>
-              <h4 class="card__heading">
-                <span class="card__heading-span card__heading-span--1">
-                  UX/UI
-                </span>
-              </h4>
-              <div class="card__details">
-                <p>view more</p>
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <div className="card">
+              <div className="card__side card__side--front">
+                <div className="card__picture card__picture--1">&nbsp;</div>
+                <p className="card__price-value">ux/ui</p>
+                <p className="card__price-only">view more</p>
               </div>
-            </div>
-            <div class="card__side card__side--back card__side--back--2">
-              <div class="card__cta">
-                <div class="card__price-box">
-                  <p class="card__price-only">only</p>
-                  <p class="card__price-value">$297</p>
+              <div className="card__side card__side--back card__side--back-1">
+                <div className="card__cta">
+                  <div className="card__price-box">
+                    <p className="card__price-only">only</p>
+                    <p className="card__price-value">$297</p>
+                  </div>
+                  <a href="#" className="btn btn--white">
+                    Book now
+                  </a>
                 </div>
-                <a href="#" class="btn btn--white">
-                  Book now
-                </a>
               </div>
             </div>
-          </div>
+          </Box>
         </Grid>
         <Grid item xs={4}>
-          <Card>
-            <CardMedia>
-              <Typography>UI/UX</Typography>
-            </CardMedia>
-          </Card>
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <div className="card">
+              <div className="card__side card__side--front">
+                <div className="card__picture card__picture--1">&nbsp;</div>
+                <p className="card__price-value">ux/ui</p>
+                <p className="card__price-only">view more</p>
+              </div>
+              <div className="card__side card__side--back card__side--back-1">
+                <div className="card__cta">
+                  <div className="card__price-box">
+                    <p className="card__price-only">only</p>
+                    <p className="card__price-value">$297</p>
+                  </div>
+                  <a href="#" className="btn btn--white">
+                    Book now
+                  </a>
+                </div>
+              </div>
+            </div>
+          </Box>
         </Grid>
         <Grid item xs={4}>
-          <Card>
-            <CardMedia>
-              <Typography>UI/UX</Typography>
-            </CardMedia>
-          </Card>
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <div className="card">
+              <div className="card__side card__side--front">
+                <div className="card__picture card__picture--1">&nbsp;</div>
+                <p className="card__price-value">ux/ui</p>
+                <p className="card__price-only">view more</p>
+              </div>
+              <div className="card__side card__side--back card__side--back-1">
+                <div className="card__cta">
+                  <div className="card__price-box">
+                    <p className="card__price-only">only</p>
+                    <p className="card__price-value">$297</p>
+                  </div>
+                  <a href="#" className="btn btn--white">
+                    Book now
+                  </a>
+                </div>
+              </div>
+            </div>
+          </Box>
         </Grid>
         <Grid item xs={4}>
-          <Card>
-            <CardMedia>
-              <Typography>UI/UX</Typography>
-            </CardMedia>
-          </Card>
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <div className="card">
+              <div className="card__side card__side--front">
+                <div className="card__picture card__picture--1">&nbsp;</div>
+                <p className="card__price-value">ux/ui</p>
+                <p className="card__price-only">view more</p>
+              </div>
+              <div className="card__side card__side--back card__side--back-1">
+                <div className="card__cta">
+                  <div className="card__price-box">
+                    <p className="card__price-only">only</p>
+                    <p className="card__price-value">$297</p>
+                  </div>
+                  <a href="#" className="btn btn--white">
+                    Book now
+                  </a>
+                </div>
+              </div>
+            </div>
+          </Box>
         </Grid>
-        <Grid item xs={4}>
-          <Card>
-            <CardMedia>
-              <Typography>UI/UX</Typography>
-            </CardMedia>
-          </Card>
+        <Grid item xs={4} >
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <div className="card">
+              <div className="card__side card__side--front">
+                <div className="card__picture card__picture--1">&nbsp;</div>
+                <p className="card__price-value">ux/ui</p>
+                <p className="card__price-only">view more</p>
+              </div>
+              <div className="card__side card__side--back card__side--back-1">
+                <div className="card__cta">
+                  <div className="card__price-box">
+                    <p className="card__price-only">only</p>
+                    <p className="card__price-value">$297</p>
+                  </div>
+                  <a href="#" className="btn btn--white">
+                    Book now
+                  </a>
+                </div>
+              </div>
+            </div>
+          </Box>
         </Grid>
       </Grid>
     </Container>
