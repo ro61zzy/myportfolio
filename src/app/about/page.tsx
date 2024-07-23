a4f0acf16feda09fb899909d3af0f26c6f493404
rozzy <rosekaremeri@gmail.com>
2024-07-23 15:02:29 +0300

    Feature: Tools

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,15 @@
+"use client";
+
 import { Box, Button, Container, Typography } from "@mui/material";
 import Image from "next/image";
 import pic from "../../../public/images.jpeg";
+import react from "../../../public/react.png";
+import next from "../../../public/next.png"
+import { useRouter } from "next/navigation";
 
 const About = () => {
+  const route = useRouter();
+
   return (
     <div>
       <Box
@@ -18,7 +25,7 @@ const About = () => {
         <Box
           sx={{
             backgroundColor: "rgba(0, 0, 0, 0.8)",
-            padding: "1rem",
+            padding: "1rem 2rem",
             width: "100%",
           }}
         >
@@ -33,18 +40,21 @@ const About = () => {
             <Box sx={{ display: "flex", flexDirection: "column" }}>
               <Image
                 src={pic}
-                alt="Picture of the author"
+                alt="rozzy"
                 width={250}
                 height={250}
                 quality={100}
                 style={{ borderRadius: "50%" }}
               />
-              <Button sx={{textTransform: "none"}}>
+              <Button
+                sx={{ textTransform: "none", mt: "1rem" }}
+                onClick={() => route.push("https://github.com/ro61zzy")}
+              >
                 <Typography
                   sx={{
                     color: "#fff",
                     fontWeight: "700",
-                    fontSize:{xs:"", sm:""}
+                    fontSize: { xs: "", sm: "25px" },
                   }}
                 >
                   GitHub
@@ -57,9 +67,7 @@ const About = () => {
                 p: "1rem",
               }}
             >
-              <Typography
-                sx={{ color: "red", fontSize: "20px", fontWeight: "500" }}
-              >
+              <Typography sx={{ fontSize: "20px", fontWeight: "500" }}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -68,6 +76,72 @@ const About = () => {
                 nulla pariatur. Excepteur sint occaecat cupidatat non proident,
                 sunt in culpa qui officia deserunt mollit anim id est laborum.
               </Typography>
+            </Box>
+          </Box>
+          <Box>
+            <Typography
+              sx={{
+                color: "#fff",
+                fontWeight: "700",
+                fontSize: { xs: "", sm: "25px" },
+                pb:"1rem"
+              }}
+            >
+              Languages and Tools
+            </Typography>
+            <Box>
+              <Button
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-evenly",
+                  textTransform: "none",
+                  backgroundColor:"#10BBE0",
+                  borderRadius:"15px",
+                  width:"18%"
+                }}
+                onClick={() => route.push("https://nextjs.org/")}
+              >
+                <Image
+                  src={react}
+                  alt="reacrt-js"
+                  width={20}
+                  height={20}
+                  quality={100}
+                  style={{ borderRadius: "50%" }}
+                />
+                <Typography
+                  sx={{ color: "#fff", fontSize: "14px", fontWeight: "600" }}
+                >
+                  Next-Js
+                </Typography>
+              </Button>
+              <Button
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-evenly",
+                  textTransform: "none",
+                  backgroundColor:"#000",
+                  borderRadius:"15px",
+                  width:"18%"
+                }}
+                onClick={() => route.push("https://react.dev/")}
+              >
+                <Image
+                  src={react}
+                  alt="reacrt-js"
+                  width={20}
+                  height={20}
+                  quality={100}
+                  style={{ borderRadius: "50%" }}
+                />
+                <Typography
+                  sx={{ color: "#fff", fontSize: "14px", fontWeight: "600" }}
+                >
+                  React-Js
+                </Typography>
+              </Button>
             </Box>
           </Box>
           <Typography
